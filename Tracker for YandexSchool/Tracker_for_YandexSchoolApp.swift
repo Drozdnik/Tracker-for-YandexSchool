39f59a2f08fbe3392283cda19e2b58930b0dcb61
Drozdnik <drozdnik@list.ru>
2024-06-16 20:54:54 +0300

    Initial Commit

---
+++ Tracker for YandexSchool/Tracker_for_YandexSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tracker_for_YandexSchoolApp.swift
+//  Tracker for YandexSchool
+//
+//  Created by Михаил  on 16.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tracker_for_YandexSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
