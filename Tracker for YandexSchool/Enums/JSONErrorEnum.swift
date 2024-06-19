c6a98afbe187a702427157982732167d9600d9fe
Drozdnik <drozdnik@list.ru>
2024-06-19 15:24:23 +0300

    refactor a bit

--- Tracker for YandexSchool/Enums/JSONErrorEnum.swift
+++ Tracker for YandexSchool/Enums/JSONErrorEnum.swift
@@ -9,4 +9,5 @@ import Foundation
 
 enum JSONErrorEnum: Error{
     case createJSONError
+    case jsonAsDataError
 }
--- Tracker for YandexSchool/Extensions/ToDoItem + extension.swift
+++ Tracker for YandexSchool/Extensions/ToDoItem + extension.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension ToDoItem: ToDoItemParseProtocol {
-
+    
     var json: Any {
         let isoFormatter = ISO8601DateFormatter.shared
         var jsonObject: [String: Any] = [
@@ -39,85 +39,84 @@ extension ToDoItem: ToDoItemParseProtocol {
     
     static func parse(json: Any) -> ToDoItem?{
         let isoFormatter = ISO8601DateFormatter.shared
-        do{
-            guard let jsonData = try JSONSerialization.jsonObject(with: json as! Data) as? [String : Any] else {
-                return nil
-            }
-
-            guard let id = jsonData["id"] as? String,
-                  let text = jsonData["text"] as? String,
-                  let flag = jsonData["flag"] as? Bool,
-                  let createdAtString = jsonData["createdAt"] as? String else {
-                return nil
-            }
-            
-            guard let createdAt = isoFormatter.date(from: createdAtString) else {
-                print ("\(createdAtString)")
-                return nil
-            }
-            
-            var changedAt: Date? = nil
-            if let changedAtTimeString = jsonData["changedAt"] as? String {
-                changedAt = isoFormatter.date(from: changedAtTimeString)
-            }
-            
-            var deadLine: Date? = nil
-            if let deadLineString = jsonData["deadLine"] as? String{
-                deadLine = isoFormatter.date(from: deadLineString)
-            }
-            
-            let priority: Priority
-            if let priorityRaw = jsonData["priority"] as? String,
-               let value = Priority(rawValue: priorityRaw){
-                priority = value
-            } else {
-                priority = Priority.normal
-            }
-            
-            return ToDoItem(
-                id: id,
-                text: text,
-                priority: priority,
-                deadLine: deadLine,
-                flag: flag,
-                createdAt: createdAt,
-                changedAt: changedAt
-            )
-        } catch {
-            assertionFailure ("\(error)")
+        guard let jsonAsData = json as? Data else {
+            assertionFailure ("\(JSONErrorEnum.jsonAsDataError)")
             return nil
         }
+        guard let jsonData = try? JSONSerialization.jsonObject(with: jsonAsData) as? [String : Any] else {
+            return nil
+        }
+        
+        guard let id = jsonData["id"] as? String,
+              let text = jsonData["text"] as? String,
+              let flag = jsonData["flag"] as? Bool,
+              let createdAtString = jsonData["createdAt"] as? String else {
+            return nil
+        }
+        
+        guard let createdAt = isoFormatter.date(from: createdAtString) else {
+            print ("\(createdAtString)")
+            return nil
+        }
+        
+        var changedAt: Date? = nil
+        if let changedAtTimeString = jsonData["changedAt"] as? String {
+            changedAt = isoFormatter.date(from: changedAtTimeString)
+        }
+        
+        var deadLine: Date? = nil
+        if let deadLineString = jsonData["deadLine"] as? String{
+            deadLine = isoFormatter.date(from: deadLineString)
+        }
+        
+        let priority: Priority
+        if let priorityRaw = jsonData["priority"] as? String,
+           let value = Priority(rawValue: priorityRaw){
+            priority = value
+        } else {
+            priority = Priority.normal
+        }
+        
+        return ToDoItem(
+            id: id,
+            text: text,
+            priority: priority,
+            deadLine: deadLine,
+            flag: flag,
+            createdAt: createdAt,
+            changedAt: changedAt
+        )
     }
     // В CSV файле пустые данные заполнены как nil
     static func parseCSV(csvString: String) -> [ToDoItem]{
         let isoFormatter = ISO8601DateFormatter.shared
         
-         var items: [ToDoItem] = []
-         let rows = csvString.split(separator: "\n")
-         
-         for row in rows.dropFirst(){
-             let columns = row.split(separator: ",").map({String($0)})
-             let id = columns[0]
-             let text = columns[1]
-             let priority = Priority(rawValue: columns[2]) ?? .normal
-             let flag = columns[3] == "true"
-             let createdAt = columns[4] != "nil" ? isoFormatter.date(from: columns[4]) : Date()
-             let deadLine = columns[5] != "nil" ? isoFormatter.date(from: columns[5]) : nil
-             let changedAt = columns[6] != "nil" ? isoFormatter.date(from: columns[6]) : nil
-             
-             let item = ToDoItem(id: id,
-                                 text: text,
-                                 priority: priority,
-                                 deadLine: deadLine,
-                                 flag: flag,
-                                 createdAt: createdAt!,
-                                 changedAt: changedAt)
-             items.append(item)
-         }
-         return items
-     
-     }
+        var items: [ToDoItem] = []
+        let rows = csvString.split(separator: "\n")
+        
+        for row in rows.dropFirst(){
+            let columns = row.split(separator: ",").map({String($0)})
+            let id = columns[0]
+            let text = columns[1]
+            let priority = Priority(rawValue: columns[2]) ?? .normal
+            let flag = columns[3] == "true"
+            let createdAt = columns[4] != "nil" ? isoFormatter.date(from: columns[4]) : Date()
+            let deadLine = columns[5] != "nil" ? isoFormatter.date(from: columns[5]) : nil
+            let changedAt = columns[6] != "nil" ? isoFormatter.date(from: columns[6]) : nil
+            
+            let item = ToDoItem(id: id,
+                                text: text,
+                                priority: priority,
+                                deadLine: deadLine,
+                                flag: flag,
+                                createdAt: createdAt!,
+                                changedAt: changedAt)
+            items.append(item)
+        }
+        return items
+        
+    }
 }
 
 
-    
+
