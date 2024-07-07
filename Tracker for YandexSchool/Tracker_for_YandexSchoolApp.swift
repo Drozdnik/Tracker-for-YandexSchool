75753dfcb63e8ff4264f2eb33a3243624250a1bc
Drozdnik <drozdnik@list.ru>
2024-07-07 15:41:44 +0300

    Added Lumberjacl to project

--- Tracker for YandexSchool/Tracker_for_YandexSchoolApp.swift
+++ Tracker for YandexSchool/Tracker_for_YandexSchoolApp.swift
@@ -1,18 +1,39 @@
 import SwiftUI
+import CocoaLumberjackSwift
 
 @main
 struct Tracker_for_YandexSchoolApp: App {
     @Environment(\.containerDI) var container
     
+    init() {
+        setupLogging()
+        DDLogInfo("Приложение запущено")
+    }
+    
     var body: some Scene {
         WindowGroup {
             if UIDevice.current.userInterfaceIdiom == .pad {
                 MainViewForIpad(viewModel: MainViewModel(fileCache: container.fileCache))
+                    .onAppear {
+                        DDLogInfo("Загрузка интерфейса для iPad")
+                    }
             } else {
                 MainView(viewModel: MainViewModel(fileCache: container.fileCache))
+                    .onAppear {
+                        DDLogInfo("Загрузка интерфейса для iPhone")
+                    }
             }
-            
-//            CalendarViewControllerRepresentable(fileCache: container.fileCache)
         }
     }
+    
+    private func setupLogging() {
+           DDLog.add(DDOSLogger.sharedInstance)
+        
+           let fileLogger: DDFileLogger = DDFileLogger()
+           fileLogger.rollingFrequency = TimeInterval(60*60*24 * 7)
+           fileLogger.logFileManager.maximumNumberOfLogFiles = 7
+           DDLog.add(fileLogger)
+           
+           dynamicLogLevel = DDLogLevel.verbose
+       }
 }
